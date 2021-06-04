1d547e4c76fa511e54b007d5644d1a39cb9c6be1
freebebe <gogofreebebe@tutamail.com>
2021-06-04 18:35:58 +1000

    up

---
+++ archDesktop/log/i915errorCPU.md
@@ -0,0 +1,4 @@
+# 
+`
+#   i915 0000:00:02.0: [drm[ *ERROR* CPU pipe A FIFO underrun
+`
--- linux/.qStart/bakUp/setup-Git.sh
+++ linux/.qStart/bakUp/setup-Git.sh
@@ -1,50 +1,50 @@
 #!/bin/sh
 
 #home_vim*1 tmux*1 zsh*1 Xmodmap*1
-cp $HOME/.vimrc $HOME/Documents/git/one/setup/linux
-cp $HOME/.tmux.conf $HOME/Documents/git/one/setup/linux
-cp $HOME/.Xmodmap $HOME/Documents/git/one/setup/linux
-cp $HOME/.gitconfig $HOME/Documents/git/one/setup/linux
-cp $HOME/.zshrc $HOME/Documents/git/one/setup/linux
-cp $HOME/.curlrc $HOME/Documents/git/one/setup/linux
-cp $HOME/.profile $HOME/Documents/git/one/setup/linux
-cp $HOME/.pam_environment $HOME/Documents/git/one/setup/linux
-cp $HOME/.Xdefaults $HOME/Documents/git/one/setup/linux              # wayland
-cp $HOME/.Xresources $HOME/Documents/git/one/setup/linux             # x10
+cp $HOME/.vimrc $HOME/one/setup/linux
+cp $HOME/.tmux.conf $HOME/one/setup/linux
+cp $HOME/.Xmodmap $HOME/one/setup/linux
+cp $HOME/.gitconfig $HOME/one/setup/linux
+cp $HOME/.zshrc $HOME/one/setup/linux
+cp $HOME/.curlrc $HOME/one/setup/linux
+cp $HOME/.profile $HOME/one/setup/linux
+cp $HOME/.pam_environment $HOME/one/setup/linux
+cp $HOME/.Xdefaults $HOME/one/setup/linux              # wayland
+cp $HOME/.Xresources $HOME/one/setup/linux             # x10
 
 #filepacker ->  /home/.config/
 ##-rf && r : skip question
-cp -r $HOME/.config/Code/User/ $HOME/Documents/git/one/setup/linux/.config/Code
-cp -r $HOME/.config/zathura/ $HOME/Documents/git/one/setup/linux/.config
-cp -r $HOME/.config/rclone/ $HOME/Documents/git/one/setup/linux/.config
-cp -r $HOME/.config/i3/ $HOME/Documents/git/one/setup/linux/.config
-cp -r $HOME/.config/sway/ $HOME/Documents/git/one/setup/linux/.config
-cp -r $HOME/.config/i3status/ $HOME/Documents/git/one/setup/linux/.config
-cp -r $HOME/.config/autostart/ $HOME/Documents/git/one/setup/linux/.config
-cp -r $HOME/.config/nvim/ $HOME/Documents/git/one/setup/linux/.config
-cp -r $HOME/.config/kitty/ $HOME/Documents/git/one/setup/linux/.config
-cp -r $HOME/.config/gtk-3.0/ $HOME/Documents/git/one/setup/linux/.config
-cp -r $HOME/.config/environment.d/ $HOME/Documents/git/one/setup/linux/.config
-cp -r $HOME/.config/aria2/ $HOME/Documents/git/one/setup/linux/.config
+cp -r $HOME/.config/Code/User/ $HOME/one/setup/linux/.config/Code
+cp -r $HOME/.config/zathura/ $HOME/one/setup/linux/.config
+cp -r $HOME/.config/rclone/ $HOME/one/setup/linux/.config
+cp -r $HOME/.config/i3/ $HOME/one/setup/linux/.config
+cp -r $HOME/.config/sway/ $HOME/one/setup/linux/.config
+cp -r $HOME/.config/i3status/ $HOME/one/setup/linux/.config
+cp -r $HOME/.config/autostart/ $HOME/one/setup/linux/.config
+cp -r $HOME/.config/nvim/ $HOME/one/setup/linux/.config
+cp -r $HOME/.config/kitty/ $HOME/one/setup/linux/.config
+cp -r $HOME/.config/gtk-3.0/ $HOME/one/setup/linux/.config
+cp -r $HOME/.config/environment.d/ $HOME/one/setup/linux/.config
+cp -r $HOME/.config/aria2/ $HOME/one/setup/linux/.config
 
 ## brower
-cp -r $HOME/.mozilla/firefox/l0gvz3le.default-release/prefs.js $HOME/Documents/git/one/setup/linux/
+cp -r $HOME/.mozilla/firefox/l0gvz3le.default-release/prefs.js $HOME/one/setup/linux/
 
 ## V3
-cp /opt/vw/config.json $HOME/Documents/git/one/setup/linux/bkup
+cp /opt/vw/config.json $HOME/one/setup/linux/bkup
 
 ## for ubuntu-desk-i3
-cp -r $HOME/.config/gtk-3.0/ $HOME/Documents/git/one/setup/ubuntuDesktop/.config
-cp -r $HOME/.Xresources $HOME/Documents/git/one/setup/ubuntuDesktop/
+cp -r $HOME/.config/gtk-3.0/ $HOME/one/setup/ubuntuDesktop/.config
+cp -r $HOME/.Xresources $HOME/one/setup/ubuntuDesktop/
 
 # #filepacker ->  /home/
-# cp -r $HOME/bkup/ $HOME/Documents/git/one/setup/
-cp -r $HOME/.qStart $HOME/Documents/git/one/setup/linux
+# cp -r $HOME/bkup/ $HOME/one/setup/
+cp -r $HOME/.qStart $HOME/one/setup/linux
 #
 # to flash
 
 # #upgrade_to_git_packge
-cd $HOME/Documents/git/one/setup/
+cd $HOME/one/setup/
 git add .
 git commit -m 'up'
 git pull origin master
